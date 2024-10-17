fedc0493da183a6e83519902ebfd561041ca35f9
TaoIkezaki <tikezaki.tos@gmail.com>
2024-10-17 17:33:13 +0900

    feat: アプリのPWA化

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,43 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "Ionic App",
+        name: "My Ionic App",
+        icons: [
+          {
+            src: "assets/icon/favicon.png",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "assets/icon/icon.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+      },
+    }),
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
